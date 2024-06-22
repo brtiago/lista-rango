ea210c57806ad25f2b4e44d0618ab44b3eb5765b
brtiago <tgribeiro@gmail.com>
2024-06-22 15:58:37 -0300

    feat: Criado o enum CategoriaProduto

---
+++ src/main/java/dev/tiago/lista_rango/model/CategoriaProduto.java
@@ -0,0 +1,25 @@
+package dev.tiago.lista_rango.model;
+
+public enum CategoriaProduto {
+    ACOMPANHAMENTOS,
+    BEBIDAS,
+    CAFES_E_CHAS,
+    CARNES,
+    CERVEJAS,
+    COQUETEIS,
+    ENTRADAS,
+    LANCHES,
+    MASSAS,
+    PEIXES_E_FRUTOS_DO_MAR,
+    PIZZAS,
+    PRATOS_PRINCIPAIS,
+    REFRIGERANTES,
+    SALADAS,
+    SANDUÍCHES,
+    SOBREMESAS,
+    SOPAS,
+    SUCOS_NATURAIS,
+    VEGETARIANO,
+    VEGANO,
+    VINHOS
+}
