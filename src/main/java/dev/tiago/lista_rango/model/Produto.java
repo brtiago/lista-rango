dfc654054d63d54c924dd44c131e6ab0c54020d5
brtiago <tgribeiro@gmail.com>
2024-06-22 15:59:23 -0300

    feat: Criado a classe Produto

---
+++ src/main/java/dev/tiago/lista_rango/model/Produto.java
@@ -0,0 +1,25 @@
+package dev.tiago.lista_rango.model;
+
+import java.awt.image.BufferedImage;
+import java.math.BigDecimal;
+
+public class Produto {
+    private Long id;
+    private BufferedImage foto;
+    private String nome;
+    private BigDecimal preco;
+    private CategoriaProduto categoriaProduto;
+    private Promocao promocao;
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", foto=" + foto +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", categoriaProduto=" + categoriaProduto +
+                ", promocao=" + promocao +
+                '}';
+    }
+}
