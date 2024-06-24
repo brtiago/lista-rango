c8186f982e76646e678b659e0d876a4215764599
brtiago <tgribeiro@gmail.com>
2024-06-24 18:06:11 -0300

    raw: implementada a documentação Swagger

--- src/main/java/dev/tiago/lista_rango/config/SwaggerConfig.java
+++ src/main/java/dev/tiago/lista_rango/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package dev.tiago.lista_rango.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Lista Rango API",
+                version = "0.1",
+                description = "API para gerenciar os restaurantes e os produtos do seu cardápio.",
+                contact = @Contact(
+                        name = "Tiago R.",
+                        email = "tgribeiro@gmail.com"
+                )
+        )
+)
 public class SwaggerConfig {
 }
