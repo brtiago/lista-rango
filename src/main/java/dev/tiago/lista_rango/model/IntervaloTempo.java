39b659565e8b7a898566a5a031f9b9380d70ca32
brtiago <tgribeiro@gmail.com>
2024-06-25 16:04:19 -0300

    feat: criando classe IntervaloTempo

---
+++ src/main/java/dev/tiago/lista_rango/model/IntervaloTempo.java
@@ -0,0 +1,33 @@
+package dev.tiago.lista_rango.model;
+
+import java.time.LocalDateTime;
+
+public class IntervaloTempo {
+    private final LocalDateTime horarioAbertura;
+    private final LocalDateTime horarioFechamento;
+
+    public IntervaloTempo(LocalDateTime horarioAbertura, LocalDateTime horarioFechamento) {
+        this.horarioAbertura = horarioAbertura;
+        this.horarioFechamento = horarioFechamento;
+        validarIntervalo();
+    }
+
+    private void validarIntervalo() {
+        if(!isIntervalValid(horarioAbertura) || !isIntervalValid(horarioFechamento)){
+            throw new IllegalArgumentException("Os horários devem estar em intervalos de 15 minutos.");
+        }
+    }
+
+    private boolean isIntervalValid(LocalDateTime dateTime) {
+        int minutes = dateTime.getMinute();
+        return minutes % 15 == 0;
+    }
+
+    public LocalDateTime getHorarioAbertura() {
+        return horarioAbertura;
+    }
+
+    public LocalDateTime getHorarioFechamento() {
+        return horarioFechamento;
+    }
+}
