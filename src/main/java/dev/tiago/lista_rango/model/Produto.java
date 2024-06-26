13d2e966165e558f80a6285d84738446b67b4703
brtiago <tgribeiro@gmail.com>
2024-06-26 14:48:36 -0300

    fix: anotando a classe com @Entity

--- src/main/java/dev/tiago/lista_rango/model/Produto.java
+++ src/main/java/dev/tiago/lista_rango/model/Produto.java
@@ -1,24 +1,29 @@
 package dev.tiago.lista_rango.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.awt.image.BufferedImage;
 import java.math.BigDecimal;
 
+@Entity
 public class Produto {
+    @Id
     private Long id;
-    private BufferedImage foto;
+    //private BufferedImage foto;
     private String nome;
     private BigDecimal preco;
     private CategoriaProduto categoriaProduto;
-    private Promocao promocao;
+    //private Promocao promocao;
 
     public String toString() {
         return "Produto{" +
                 "id=" + id +
-                ", foto=" + foto +
+                ", foto=" +
                 ", nome='" + nome + '\'' +
                 ", preco=" + preco +
                 ", categoriaProduto=" + categoriaProduto +
-                ", promocao=" + promocao +
+                ", promocao=" +
                 '}';
     }
 
--- src/main/java/dev/tiago/lista_rango/model/Restaurante.java
+++ src/main/java/dev/tiago/lista_rango/model/Restaurante.java
@@ -1,39 +1,31 @@
 package dev.tiago.lista_rango.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 import java.awt.image.BufferedImage;
 import java.time.LocalDateTime;
 import java.util.List;
-
+@Entity
 public class Restaurante {
+    @Id
     private Long id;
-    private BufferedImage foto;
+    //private BufferedImage foto;
     private String nome;
     private String endereco;
     private LocalDateTime funcionamento;
+    @OneToMany
     private List<Produto> produtos;
 
     public String toString() {
         return "Restaurante{" +
                 "id=" + id +
-                ", foto=" + foto +
+                ", foto=" +
                 ", nome='" + nome + '\'' +
                 ", endereco='" + endereco + '\'' +
                 ", funcionamento='" + funcionamento + '\'' +
                 ", produtos=" + produtos +
                 '}';
-    }
-
-    public Restaurante(
-            BufferedImage foto,
-            String nome,
-            String endereco,
-            LocalDateTime funcionamento,
-            List<Produto> produtos) {
-
-        this.foto = foto;
-        this.nome = nome;
-        this.endereco = endereco;
-        this.funcionamento = funcionamento;
-        this.produtos = produtos;
     }
 }
