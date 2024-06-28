1925b501c935a2a6548ad21cdc91341194621051
brtiago <tgribeiro@gmail.com>
2024-06-28 16:06:31 -0300

    feat: inclusão de variaveis locais

--- src/main/java/dev/tiago/lista_rango/model/HorarioFuncionamento.java
+++ src/main/java/dev/tiago/lista_rango/model/HorarioFuncionamento.java
@@ -1,8 +1,26 @@
 package dev.tiago.lista_rango.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalTime;
 import java.util.Map;
 
+@Entity
 public class HorarioFuncionamento {
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    Restaurante restaurante;
+
+    DiaSemana dia;
+
+    private LocalTime aberturaa;
+
+    private LocalTime fechamento;
 
     private Map<DiaSemana, IntervaloTempo> horarios;
 }
