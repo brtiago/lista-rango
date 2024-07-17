9b183c12fbaee678606ccfe6b80b9ce07a720a9c
brtiago <tgribeiro@gmail.com>
2024-07-17 05:32:12 -0300

    Rename

--- src/main/java/dev/tiago/lista_rango/repository/RestauranteRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.tiago.lista_rango.repository;
-
-import dev.tiago.lista_rango.model.Restaurante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
-}
